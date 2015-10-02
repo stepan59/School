9ecae81b89e7dca59bf2b60b5d59c460c36df64d
stepan <govorovstepan@gmail.com>
2015-10-02 17:17:57 +0700

    Закончил первое задание

--- N1.py
+++ N1.py
@@ -42,5 +42,19 @@ for el in ls:
         print(el)
 
 print()
+i = 0
 teachers_name = input("Введите имя учителя - ")
 teachers_surname = input("Введите фамилию учителя - ")
+for el in d:
+    if el['name'] == teachers_name and el['surname'] == teachers_surname:
+        i = 1
+        break
+    else:
+        i = 0
+    print(i)
+if i != 1:
+    print("Нет такого учителя")
+else:
+    print('Все в порядке')
+
+print()
--- n.py
+++ n.py
@@ -4,3 +4,19 @@ f = open('Students.json', 'r')
 file = open('Teachers.json', 'r')
 data = json.load(f)
 d = json.load(file)
+i = 0
+teachers_name = input("Введите имя учителя - ")
+teachers_surname = input("Введите фамилию учителя - ")
+for el in d:
+    if el['name'] == teachers_name and el['surname'] == teachers_surname:
+        i = 1
+        break
+    else:
+        i = 0
+print(i)
+if i != 1:
+    print("Нет такого учителя")
+else:
+    print('Все в порядке')
+
+print()
