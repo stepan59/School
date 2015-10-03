2f055d686c6888f368a11cbb7287a61fab9c9f0a
stepan <govorovstepan@gmail.com>
2015-10-03 17:42:38 +0700

    Закончил 2.2

--- N1.py
+++ N1.py
@@ -1,33 +1,43 @@
 import json
+import os
+DIR = ''
 
-f = open('Students.json', 'r')
-file = open('Teachers.json', 'r')
-data = json.load(f)
-d = json.load(file)
+def get_teachers(teacher_name,teacher_data):
+    name, surname = teacher_name.split(" ")
+    for teacher in teacher_data:
+        if teacher["name"] == name and teacher["surname"] == surname:
+            return teacher
+
+def save(data,file_name):
+    file = open(os.path.join(DIR, file_name), 'w', encoding='utf8')
+    file.write(json.dumps(data, ensure_ascii=False))
+    file.close()
+
+students_data = json.load(open(os.path.join(DIR, 'Students.json'), 'r'))
+teachers_data = json.load(open(os.path.join(DIR, 'Teachers.json'), 'r'))
 clasnum = input('Введите номер желаемого класса :')
 
-# dd
 
 print()
 print('Все ученики -')
-for el in data:
+for el in students_data:
     print(el['name'])
 
 print()
 print('Учетеля -')
-for el in d:
+for el in teachers_data:
     print(el['name'])
 
 print()
 print('Ученики из заданого класса - ')
-for el in data:
+for el in students_data:
     if (el['class']) == clasnum:
         print(el['name'], el['surname'])
 
 print()
 school = []
 print('Номера школ -')
-for el in d:
+for el in teachers_data:
     school.append(el['school'])
 for el in set(school):
     print(el)
@@ -35,7 +45,7 @@ for el in set(school):
 print()
 ls = []
 print('Однофамильцы - ')
-for el in data:
+for el in students_data:
     ls.append(el['surname'])
 for el in ls:
     if ls.count(el) > 1:
@@ -43,18 +53,44 @@ for el in ls:
 
 print()
 i = 0
-teachers_name = input("Введите имя учителя - ")
-teachers_surname = input("Введите фамилию учителя - ")
-for el in d:
-    if el['name'] == teachers_name and el['surname'] == teachers_surname:
-        i = 1
-        break
-    else:
-        i = 0
-    print(i)
-if i != 1:
-    print("Нет такого учителя")
-else:
-    print('Все в порядке')
+# teachers_name = input("Введите имя учителя - ")
+# teachers_surname = input("Введите фамилию учителя - ")
+# teachers_name = 'Александр'
+# teachers_surname = "Черный"
+# for el in teachers_data:
+#     if el['name'] == teachers_name and el['surname'] == teachers_surname:
+#         i = 1
+#         clas = el['class']
+#         print(clas)
+#         break
+#     else:
+#         i = 0
+# if i != 1:
+#     print("Нет такого учителя")
+# else:
+#     print('Все в порядке')
 
-print()
+student = {'name': 'Петр',
+                 'middle_name': 'Сергеевич',
+                 'surname': 'Иванов',
+                 "school": "12 гимназия",
+                 "class": "7 В",
+                 "birth_day": "21.05.1995"}
+students_data.append(student)
+
+save(students_data, 'Students_new.json')
+
+teacher = {
+    "name": "Максим",
+    "middle_name": "Евгеньевич",
+    "surname": "Козлов",
+    "school": "67 школа",
+    "class": [
+      "5 А",
+      "6 Б",
+      "7 В"
+    ],
+    "birth_day": "07.08.1985"
+  }
+teachers_data.append(teacher)
+save(teachers_data, 'Teachers_new.json')
