8e4b75b047fb86be22bacdd29552364a4f08b6d5
stepan <govorovstepan@gmail.com>
2015-10-10 17:37:54 +0700

    Осталось 3.4 и 3.8

--- N1.py
+++ N1.py
@@ -2,18 +2,50 @@ import json
 import os
 DIR = ''
 
-def get_teachers(teacher_name,teacher_data):
+def get_teachers(teacher_name, teacher_data):
     name, surname = teacher_name.split(" ")
     for teacher in teacher_data:
         if teacher["name"] == name and teacher["surname"] == surname:
             return teacher
 
-def save(data,file_name):
+def save(data, file_name):
     file = open(os.path.join(DIR, file_name), 'w', encoding='utf8')
     file.write(json.dumps(data, ensure_ascii=False))
     file.close()
+# уничтожение ученика
+def expel(students_class, students_data, students_list):
+    for el in students_data:
+        if el['class'] != students_class:
+            students_list.append(el)
+        else:
+            print(el['name'], el['middle_name'], el['surname'])
+            print('Выгнан')
+        save(students_list, 'Students_new.json')
+# увольнение учителя
+def dismissal(school, teachers_data, teachers_list):
+    for el in teachers_data:
+        if el['school'] != school:
+            teachers_list.append(el)
+        else:
+            print(el['name'], el['middle_name'], el['surname'])
+            print('Уволен')
+        save(teacher_list, 'Teachers_new.json')
 
-
+def expel_by_name(students_data, students_name, students_middle_name, students_srn):
+    for el in students_data:
+        if students_name == el['name'] and students_srn == el['surname'] and students_middle_name == el['middle_name']:
+            students_data.remove(el)
+            print(el['name'], el['surname'], el['middle_name'])
+            print("Выгнан")
+        save(students_data, 'Students_new.json')
+def dismissal_by_name(teachers_data, teacher_name, teacher_middle_name, teacher_srn):
+    class_list = ['6 А', "8 Б", "7 В", '7 А']
+    for el in teachers_data:
+        if teacher_name == el['name'] and teacher_srn == el['surname'] and teacher_middle_name == el['middle_name']:
+            teachers_data.remove(el)
+            print(el['name'], el['middle_name'], el['surname'])
+            print('Уволен')
+            save(teachers_data, 'Teachers_new.json')
 
 students_data = json.load(open(os.path.join(DIR, 'Students.json'), 'r'))
 teachers_data = json.load(open(os.path.join(DIR, 'Teachers.json'), 'r'))
@@ -71,7 +103,9 @@ i = 0
 #     print("Нет такого учителя")
 # else:
 #     print('Все в порядке')
-# 2.1 добавления нового ученика
+
+
+# 3.1 добавления нового ученика
 student = {'name': 'Петр',
             "middle_name": 'Сергеевич',
             "surname": 'Иванов',
@@ -79,7 +113,7 @@ student = {'name': 'Петр',
             "class": "7 В",
             "birth_day": "21.05.1995"}
 students_data.append(student)
-# 2.2  добавление учителей
+# 3.2  добавление учителей
 save(students_data, 'Students_new.json')
 
 teacher = {
@@ -97,22 +131,47 @@ teacher = {
 teachers_data.append(teacher)
 save(teachers_data, 'Teachers_new.json')
 
-# 2.3 добавлять классы (которые он ведет) к указанному преподавателю
+# 3.3 добавлять классы (которые он ведет) к указанному преподавателю
+print('3.3 добавлять классы (которые он ведет) к указанному преподавателю')
 teachers_data_n = json.load(open(os.path.join(DIR, 'Teachers_new.json'), 'r'))
 teacher_name = "Максим"
 teacher_srn = "Козлов"
-class_list = ['6 А', "8 Б", "7 В", '7 А']
+added_class = '4 А'
 for el in teachers_data_n:
     if teacher_name == el['name'] and teacher_srn == el['surname']:
-        el['class'] = class_list
-        save(teachers_data_n, 'Teachers_new.json')
+        el['class'].append(added_class)
+        print(el['class'])
+    save(teachers_data_n, 'Teachers_new.json')
+print('Успех')
 
-# 2.4 Добавьте возможность удаления ученика по заданному полному имени
+# 3.4 Добавьте возможность удаления ученика по заданному полному имени
+print('3.4 Добавьте возможность удаления ученика по заданному полному имени')
+print()
 students_data_n = json.load(open(os.path.join(DIR, 'Students_new.json'), 'r'))
-students_name = "Артем"
-students_middle_name = "Валерьевич"
-students_srn = "Кузин"
-for el in students_data:
-    if students_name == el['name'] and students_srn == el['surname'] and students_middle_name == el['middle_name']:
-        students_data.remove(el)
-        save(students_data, 'Students_new.json')
+expel_by_name(students_data_n, "Артем", "Валерьевич", "Кузин")
+print()
+
+# 3.5 удаление всех учеников из класса
+print('3.5 удаление всех учеников из класса')
+print()
+students_list = []
+expel('6 А', students_data, students_list)
+print()
+
+# 3.6 удаление учителя по полному имени(Ф.И.О.)
+print('3.6 удаление учителя по полному имени(Ф.И.О.)')
+print()
+teachers_data_n = json.load(open(os.path.join(DIR, 'Teachers_new.json'), 'r'))
+dismissal_by_name(teachers_data_n, "Александр", "Сергеевич", "Черный")
+print()
+
+# 3.7 удаления всех учителей из указанной школы
+print('3.7 удаления всех учителей из указанной школы')
+print()
+teacher_list = []
+dismissal("12 гимназия", teachers_data, teacher_list)
+print()
+
+# 3.8 удаление указанных классов у указанного преподавателя
+
+
