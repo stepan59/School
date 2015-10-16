693f90e384a0932feee8c37d2d102af78106bab0
stepan <govorovstepan@gmail.com>
2015-10-16 17:38:29 +0700

    Начало 4

--- N1.py
+++ N1.py
@@ -2,11 +2,9 @@ import json
 import os
 DIR = ''
 
-def get_teachers(teacher_name, teacher_data):
-    name, surname = teacher_name.split(" ")
-    for teacher in teacher_data:
-        if teacher["name"] == name and teacher["surname"] == surname:
-            return teacher
+def full_name(data):
+    for el in data:
+        print(el['name'], el['middle_name'], el['surname'])
 
 def save(data, file_name):
     file = open(os.path.join(DIR, file_name), 'w', encoding='utf8')
@@ -39,7 +37,6 @@ def expel_by_name(students_data, students_name, students_middle_name, students_s
             print("Выгнан")
         save(students_data, 'Students_new.json')
 def dismissal_by_name(teachers_data, teacher_name, teacher_middle_name, teacher_srn):
-    class_list = ['6 А', "8 Б", "7 В", '7 А']
     for el in teachers_data:
         if teacher_name == el['name'] and teacher_srn == el['surname'] and teacher_middle_name == el['middle_name']:
             teachers_data.remove(el)
@@ -47,20 +44,24 @@ def dismissal_by_name(teachers_data, teacher_name, teacher_middle_name, teacher_
             print('Уволен')
             save(teachers_data, 'Teachers_new.json')
 
-students_data = json.load(open(os.path.join(DIR, 'Students.json'), 'r'))
-teachers_data = json.load(open(os.path.join(DIR, 'Teachers.json'), 'r'))
+students_data = json.load(open(os.path.join(DIR, 'Students_id.json'), 'r'))
+teachers_data = json.load(open(os.path.join(DIR, 'Teachers_id.json'), 'r'))
 clasnum = input('Введите номер желаемого класса :')
+# поседний id студентов
+for el in students_data:
+   last_students_id = el['id']
+# поседний id учеников
+for el in teachers_data:
+   last_teachers_id = el['id']
 
 
 print()
 print('Все ученики -')
-for el in students_data:
-    print(el['name'])
+full_name(students_data)
 
 print()
-print('Учетеля -')
-for el in teachers_data:
-    print(el['name'])
+print('Учителя -')
+full_name(teachers_data)
 
 print()
 print('Ученики из заданого класса - ')
@@ -84,39 +85,19 @@ for el in students_data:
 for el in ls:
     if ls.count(el) > 1:
         print(el)
-
-print()
-i = 0
-# teachers_name = input("Введите имя учителя - ")
-# teachers_surname = input("Введите фамилию учителя - ")
-# teachers_name = 'Александр'
-# teachers_surname = "Черный"
-# for el in teachers_data:
-#     if el['name'] == teachers_name and el['surname'] == teachers_surname:
-#         i = 1
-#         clas = el['class']
-#         print(clas)
-#         break
-#     else:
-#         i = 0
-# if i != 1:
-#     print("Нет такого учителя")
-# else:
-#     print('Все в порядке')
-
-
+        
 # 3.1 добавления нового ученика
-student = {'name': 'Петр',
+student = {'id': last_students_id+1,
+            'name': 'Петр',
             "middle_name": 'Сергеевич',
             "surname": 'Иванов',
             "school": "12 гимназия",
             "class": "7 В",
             "birth_day": "21.05.1995"}
 students_data.append(student)
-# 3.2  добавление учителей
 save(students_data, 'Students_new.json')
-
-teacher = {
+# 3.2  добавление учителей
+teacher = {'id' : last_teachers_id+1,
     "name": "Максим",
     "middle_name": "Евгеньевич",
     "surname": "Козлов",
@@ -133,22 +114,18 @@ save(teachers_data, 'Teachers_new.json')
 
 # 3.3 добавлять классы (которые он ведет) к указанному преподавателю
 print('3.3 добавлять классы (которые он ведет) к указанному преподавателю')
-teachers_data_n = json.load(open(os.path.join(DIR, 'Teachers_new.json'), 'r'))
-teacher_name = "Максим"
-teacher_srn = "Козлов"
+teacher_id = "4"
 added_class = '4 А'
-for el in teachers_data_n:
-    if teacher_name == el['name'] and teacher_srn == el['surname']:
+for el in teachers_data:
+    if teacher_id == el['id']:
         el['class'].append(added_class)
-        print(el['class'])
-    save(teachers_data_n, 'Teachers_new.json')
+save(teachers_data, 'Teachers_new.json')
 print('Успех')
 
 # 3.4 Добавьте возможность удаления ученика по заданному полному имени
 print('3.4 Добавьте возможность удаления ученика по заданному полному имени')
 print()
-students_data_n = json.load(open(os.path.join(DIR, 'Students_new.json'), 'r'))
-expel_by_name(students_data_n, "Артем", "Валерьевич", "Кузин")
+expel_by_name(students_data, "Артем", "Валерьевич", "Кузин")
 print()
 
 # 3.5 удаление всех учеников из класса
@@ -161,8 +138,7 @@ print()
 # 3.6 удаление учителя по полному имени(Ф.И.О.)
 print('3.6 удаление учителя по полному имени(Ф.И.О.)')
 print()
-teachers_data_n = json.load(open(os.path.join(DIR, 'Teachers_new.json'), 'r'))
-dismissal_by_name(teachers_data_n, "Александр", "Сергеевич", "Черный")
+dismissal_by_name(teachers_data, "Александр", "Сергеевич", "Черный")
 print()
 
 # 3.7 удаления всех учителей из указанной школы
@@ -173,5 +149,13 @@ dismissal("12 гимназия", teachers_data, teacher_list)
 print()
 
 # 3.8 удаление указанных классов у указанного преподавателя
+print('3.8 удаление указанных классов у указанного преподавателя')
+teacher_id = "1"
+del_class = '6 А'
+for el in teachers_data:
+    if teacher_id == el['id']:
+        el['class'].remove(del_class)
+save(teachers_data, 'Teachers_new.json')
+print('Успех')
 
 
